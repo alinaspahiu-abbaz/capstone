5c640dc69d5451be2c1f4b7aa3880e5ebb9d353b
AlinaSpahiu <alina.s.abbaz@gmail.com>
2020-09-22 23:00:13 +0200

    HomePage

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import Hero from "../components/Hero"
+import Banner from "../components/Banner"
+import {Link} from "react-router-dom"
+import Services from "../components/Services"
+
+export default function Home(){
+    return(
+         <>
+          <Hero>
+            <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
+                <Link to="/rooms" className="btn-primary">
+                    Our rooms
+                </Link>
+            </Banner>
+
+          </Hero>
+            <Services />
+          </>
+    )
+}
