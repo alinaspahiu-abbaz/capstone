df2fcaa3943530f8bfda0796eebb0fd2fa09a50e
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-09-23 23:18:15 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router } from "react-router-dom"
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {RoomProvider} from "./context"
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <RoomProvider>
+     <Router>
+        <App />
+     </Router>
+  </RoomProvider>,
   document.getElementById('root')
 );
 
