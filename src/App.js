8ec244815cd6762717b7c0531be6ba81d1ef4d56
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-09-23 23:16:43 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/rooms" component={Rooms} />
-          <Route exact path="/rooms/:single" component={SingleRoom} />
+          <Route exact path="/rooms/:slug" component={SingleRoom} />
            {/* path="/rooms/:slug" Route parameter */}
           <Route component={Error} />
         </Switch>  
