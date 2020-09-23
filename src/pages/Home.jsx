79f0fceda13cf80c8b6b8d7f884a6ebd6ed0641c
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-09-23 23:18:24 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import Hero from "../components/Hero"
 import Banner from "../components/Banner"
 import {Link} from "react-router-dom"
 import Services from "../components/Services"
+import FeaturedRooms from "../components/FeaturedRoom"
+
 
 export default function Home(){
     return(
@@ -16,6 +18,8 @@ export default function Home(){
 
           </Hero>
             <Services />
+            <FeaturedRooms />
+           
           </>
     )
 }
