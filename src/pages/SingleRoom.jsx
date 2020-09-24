8e16fbfb0c9553fd0152c9fd51fc5c77369b0d52
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-09-24 23:00:34 +0200

    Single Room Up[dated

--- src/pages/SingleRoom.jsx
+++ src/pages/SingleRoom.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react"
-import defaultBcg from "../images/room-1.jpeg"
+import defaultBcg from "../images/room-3.jpeg"
 import Hero from "../components/Hero"
 import Banner from "../components/Banner"
 import {Link} from "react-router-dom"
@@ -9,19 +9,20 @@ import StyledHero from "../components/StyledHero"
 export default class SingleRoom extends Component {
     constructor (props){
         super(props)
-        // console.log(this.props)
+         console.log(this.props)
         this.state={
             slug: this.props.match.params.slug,
-            defaultBcg
+            defaultBcg: defaultBcg[0]
         }
+        console.log(defaultBcg)
     }
 
     static contextType = RoomContext;
 
     // ComponentDidMount()
-    componentDidMount() {
+    //componentDidMount() {
 
-    }
+   // }
     render(){
         const {getRoom} = this.context;
         const room = getRoom(this.state.slug);
@@ -33,12 +34,49 @@ export default class SingleRoom extends Component {
                    </div>
         }
         const {name, description, capacity, size, price, extras, breakfast, pets, images} = room
-    return (
-        <StyledHero img={images[0]}>
+        const [mainImg, ...defaultImg] = images;
+        console.log("default Images: ", defaultImg);
+        return (
+            <>
+            <StyledHero img={mainImg || this.state.defaultBcg}>
             <Banner title={`${name} room`}>
-               <Link to="/rooms" className="btn-primary"> Back to rooms</Link>
+              <Link to="/rooms" className="btn-primary">
+                back to rooms
+              </Link>
             </Banner>
-        </StyledHero>
+          </StyledHero>
+
+          <section className="single-room">
+             <div className="single-room-images">
+             {defaultImg.map((item, index) => {
+                 return <img key={index} src={item} alt={name} />
+             })
+             }
+                 </div> 
+                 <div className="single-room-info">
+                     <article className="desc">
+                         <h3> details: </h3>
+                         <p> {description} </p>
+                     </article>
+                     <article className="info">
+                         <h3> info: </h3>
+                         <h6> price: ${price}</h6>
+                         <h6> size: {size} SQFT</h6>
+                         <h6> max capacirty: { capacity>1? `${capacity} people`: `${capacity}person`}</h6>
+                         <h6> {pets ? "pets allowed" : "no pets allowed"} </h6>
+                         <h6> {breakfast && "free breakfast included"} </h6>
+                     </article>
+                 </div>
+          </section>
+          <section className="room-extras">
+              <h6> extras: </h6>
+              <ul className="extras">
+                  {extras.map((item, index) => {
+                  return <li key={index}> - {item}</li> 
+                })}
+              </ul>
+          </section>
+          </>
     )
     }
 }
