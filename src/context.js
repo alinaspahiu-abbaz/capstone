3204063698829688d45089fc9bc17aaa7128ced8
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-09-24 23:00:20 +0200

    Update context.js

--- src/context.js
+++ src/context.js
@@ -19,15 +19,16 @@ const RoomContext = React.createContext()
         })
     }
 
-    formatData(items){
-        let tempItems = items.map( item =>{
-            let id = item.sys.id;
-            let images = item.fields.images.map(image => image.fields.file.url)
-            let room = { ...item.fields, images: images, id }
-            return room;
-        })
-            return tempItems;
-    }
+    formatData(items) {
+        let tempItems = items.map(item => {
+          let id = item.sys.id;
+          let images = item.fields.images.map(image => image.fields.file.url);
+    
+          let room = { ...item.fields, images, id };
+          return room;
+        });
+        return tempItems;
+      }
     
     // getRoom function
     getRoom = (slug) => {
