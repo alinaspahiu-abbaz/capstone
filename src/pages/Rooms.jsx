862890d1f9a021b5f0d39583344b3b1c8528d369
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-09-24 23:25:56 +0200

    Rooms Updated with RoomsContainer

--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -2,15 +2,18 @@ import React from "react"
 import Hero from "../components/Hero"
 import Banner from "../components/Banner"
 import {Link} from"react-router-dom"
-
+import RoomsContainer from "../components/RoomsContainer"
 const Rooms = () => {
-    return <Hero hero="roomsHero">
-             <Banner title="our rooms" >
-                <Link to="/" className="btn-primary">
-                   return home
-               </Link>
-             </Banner>
-           </Hero>
+    return<>
+        <Hero hero="roomsHero">
+          <Banner title="our rooms" >
+            <Link to="/" className="btn-primary">
+                return home
+            </Link>
+          </Banner>
+        </Hero>
+          <RoomsContainer />
+           </>
 }
 
 export default Rooms
