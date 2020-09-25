9fa6732ddca41b36b8ce44f8a44451536ad429a5
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-09-25 09:38:38 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,6 +24,7 @@ export default class Navbar extends Component {
                   <ul className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}> 
                   <li> <Link to="/"> Home </Link></li>
                   <li> <Link to="/rooms"> Rooms </Link></li>
+                  <li> <Link to="/spa"> Spa </Link></li>
                   </ul>
                 </div>
             </nav>
