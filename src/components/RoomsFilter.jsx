b60e425dd9962ac37f8042d56943bcd234f89bfb
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-09-25 02:22:17 +0200

    Update RoomsFilter.jsx

--- src/components/RoomsFilter.jsx
+++ src/components/RoomsFilter.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from 'react';
+import {useContext} from 'react';
+import {RoomContext} from "../context";
+import Title from "../components/Title";
 
-export default function RoomsFilter() {
+//get all unique values
+const getUnique = (items, value) => {
+  return [... new Set(items.map(item =>item[value]))]
+}
+
+
+export default function RoomsFilter({rooms}) {
+    const context = useContext(RoomContext);
+    console.log(context)
+
+    const{
+           handleChange, type, capacity, price, 
+           minPrice, maxPrice, minSize, maxSize, breakfast, pets
+        } = context;
+
+    
+    let types = getUnique(rooms, 'type');// get unique types: 
+    types = ['all', ...types]; // add all
+    types = types.map((item, index) =>{ // map to  jsx:
+           return <option value={item} key={index}>{item}</option>
+       })
+    
+    let people = getUnique(rooms, 'capacity');
+    people = people.map((item, index) => { return <option key={index} value={item}>{item}</option>})
+
     return (
-        <div>
-           Hello from Rooms  Filter 
-        </div>
+        <section className="filter-container">
+           <Title title="search rooms" />
+           <form className="filter-form">
+               {/* select type: */}
+               <div className="form-group">
+                   <label htmlFor="type"> room type</label>
+                   <select name="type" id="type" value={type} className="form-control" onChange={handleChange}>
+                     {types}
+                   </select>
+               </div>
+               {/* end of select type! */}
+
+               {/* guests: */}
+               <div className="form-group">
+                   <label htmlFor="capacity"> Guests</label>
+                   <select name="capacity" id="capacity" value={capacity} className="form-control" onChange={handleChange}>
+                     {people}
+                   </select>
+               </div>
+               {/* end of guests! */}
+           </form>
+        </section>
     )
 }
