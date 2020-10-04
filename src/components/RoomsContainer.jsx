50f0bbc651315b59a045cc75e8851d2ad9c2f9a0
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-10-04 20:16:15 +0200

    Update RoomsContainer.jsx

--- src/components/RoomsContainer.jsx
+++ src/components/RoomsContainer.jsx
@@ -1,23 +1,54 @@
+// import React, {Component} from "react";
+// import { RoomConsumer } from "../context";
+// import Loading from "./Loading";
+// import RoomsFilter from "./RoomsFilter";
+// import RoomsList from "./RoomsList";
+// import {Container, Row, Col, Button} from "react-bootstrap"
+
+
+
+// export default class RoomsContainer extends Component {
+//     state={
+//         rooms: [],
+        
+//     }
+//     render(){
+//     return (<> 
+//     <Container>
+//         <div>
+//             {this.state.rooms.map( room =>
+//             <RoomsList />
+//                 // <Row className="">
+//                 //     <Col sm={2}>{room.name}</Col>
+//                 // </Row>
+//                 )
+//             }
+//         </div>
+//     </Container>
+
+
+        
+        
+//         </>
+//     )
+// }
+// componentDidMount = async () => {
+//     const roomsResp = await fetch("http://localhost:3003/rooms")
+//     const rooms = await roomsResp.json()
+//     this.setState({
+//         rooms: rooms
+//     })
+//     console.log(rooms)
+// }
+// }
+
 import React from "react";
 import { withRoomConsumer, RoomConsumer } from "../context";
 import Loading from "./Loading";
 import RoomsFilter from "./RoomsFilter";
 import RoomsList from "./RoomsList";
 
-// function RoomContainer({ context }) {
-//   const { loading, sortedRooms, rooms } = context;
-//   if (loading) {
-//     return <Loading />;
-//   }
-//   return (
-//     <>
-//       <RoomsFilter rooms={rooms} />
-//       <RoomsList rooms={sortedRooms} />
-//     </>
-//   );
-// }
 
-// export default withRoomConsumer(RoomContainer);
 export default function RoomsContainer() {
     return (<> 
         <RoomConsumer>
