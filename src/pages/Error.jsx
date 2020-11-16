41d6d4925086d1a13244f34637c51832659a48fe
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-11-16 23:49:19 +0100

    Update Error.jsx

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -3,6 +3,7 @@ import Hero from "../components/Hero"
 import Banner from "../components/Banner"
 import {Link} from"react-router-dom"
 
+
 export default function Error(){
     return(
        <Hero>
